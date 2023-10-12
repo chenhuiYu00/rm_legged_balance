fddbc2174ce174118201b1e17db4f0a4c6de1747
yuchen <116358@qq>
2023-10-13 04:45:00 +0800

    Update visualization,incomplete.

--- rm_legged_balance_control_ros/src/LeggedBalanceVisualization.cpp
+++ rm_legged_balance_control_ros/src/LeggedBalanceVisualization.cpp
@@ -18,7 +18,7 @@ void LeggedBalanceVisualization::update(const ocs2::SystemObservation& observati
   geometry_msgs::TransformStamped world_transform;
   world_transform.header.stamp = timeMsg;
 
-  scalar_t vel = observation.state(3);
+  scalar_t vel = observation.state(5);
   scalar_t dt = observation.time - lastObservation_.time;
   position_(0) += vel * dt * cos(observation.state(4));
   position_(1) += vel * dt * sin(observation.state(4));
@@ -27,33 +27,45 @@ void LeggedBalanceVisualization::update(const ocs2::SystemObservation& observati
   // todo: leg tf
   scalar_t l_1 = 0.2;
   tf::Transform transform;
-  transform.setOrigin(tf::Vector3(position_(0) + l_1 * sin(observation.state(1)) * cos(observation.state(3)),
-                                  position_(1) + l_1 * sin(observation.state(1)) * sin(observation.state(3)),
-                                  l_1 * cos(observation.state(1)) + param_.r_));
-  tf::Quaternion q = tf::createQuaternionFromRPY(0, observation.state(1), observation.state(3));
+  transform.setOrigin(tf::Vector3(
+      position_(0) + param_.l_c * sin(observation.state(3)) *
+                         cos(observation.state(4)),  // l_1 * sin(observation.state(1)) * cos(observation.state(4)) +
+      position_(1) + param_.l_c * sin(observation.state(3)) *
+                         sin(observation.state(4)),  // l_1 * sin(observation.state(1)) * sin(observation.state(4)) +
+      (l_1 * cos(observation.state(1)) + l_1 * cos(observation.state(2))) / 2 + param_.r_ + param_.l_c * cos(observation.state(4))));
+  tf::Quaternion q = tf::createQuaternionFromRPY(atan((l_1 * cos(observation.state(2)) - l_1 * cos(observation.state(1))) / param_.d_),
+                                                 observation.state(3), observation.state(4));
   transform.setRotation(q);
   odomTransform_ = transform;
 
   if (isDummy_) {  // for dummy node visualization
     tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_link"));
 
-    transform.setOrigin(tf::Vector3(0, param_.d_ / 2, -l_1));
-    transform.setRotation(tf::createQuaternionFromRPY(
-        0, observation.state(0) / param_.r_ - observation.state(1) - observation.state(2) * param_.d_ / 2 / param_.r_, 0));
-    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "left_wheel"));
+    transform.setOrigin(tf::Vector3(0, param_.d_ / 2, 0));
+    transform.setRotation(tf::createQuaternionFromRPY(0, observation.state(3) - observation.state(1), 0));
+    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "left_leg"));
 
-    transform.setOrigin(tf::Vector3(position_(0) + param_.d_ / 2 * sin(observation.state(2)),
-                                    position_(1) - param_.d_ / 2 * cos(observation.state(2)), param_.r_));
+    transform.setOrigin(tf::Vector3(0, -param_.d_ / 2, 0));
+    transform.setRotation(tf::createQuaternionFromRPY(0, observation.state(3) - observation.state(2), 0));
+    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "right_leg"));
+
+    transform.setOrigin(tf::Vector3(0, 0, l_1));
     transform.setRotation(tf::createQuaternionFromRPY(
-        0, observation.state(0) / param_.r_ + observation.state(2) * param_.d_ / 2 / param_.r_, observation.state(2)));
-    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "right_wheel"));
+        0, observation.state(0) / param_.r_ - observation.state(1) - observation.state(4) * param_.d_ / 2 / param_.r_, 0));
+    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "left_leg", "left_wheel"));
+
+    transform.setOrigin(tf::Vector3(position_(0) + param_.d_ / 2 * sin(observation.state(4)),
+                                    position_(1) - param_.d_ / 2 * cos(observation.state(4)), param_.r_));
+    transform.setRotation(tf::createQuaternionFromRPY(
+        0, observation.state(0) / param_.r_ - observation.state(2) + observation.state(4) * param_.d_ / 2 / param_.r_, 0));
+    tfBroadcaster_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "right_leg", "right_wheel"));
   }
 
   // Power
   ocs2::vector_t input = policy.inputTrajectory_[0];
 
-  scalar_t wheel_left_speed = observation.state(3) / param_.r_ - observation.state(4) - observation.state(5) * param_.d_ / 2 / param_.r_;
-  scalar_t wheel_right_speed = observation.state(3) / param_.r_ - observation.state(4) + observation.state(5) * param_.d_ / 2 / param_.r_;
+  scalar_t wheel_left_speed = observation.state(5) / param_.r_ - observation.state(6) - observation.state(9) * param_.d_ / 2 / param_.r_;
+  scalar_t wheel_right_speed = observation.state(5) / param_.r_ - observation.state(7) + observation.state(9) * param_.d_ / 2 / param_.r_;
   scalar_t power = abs(input(0) * wheel_left_speed) + param_.powerCoeffEffort_ * input(0) * input(0) +
                    param_.powerCoeffVel_ * wheel_left_speed * wheel_left_speed + abs(input(1) * wheel_right_speed) +
                    param_.powerCoeffEffort_ * input(1) * input(1) + param_.powerCoeffVel_ * wheel_right_speed * wheel_right_speed +
