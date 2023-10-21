0029058d9398b0488357e0f530b30d782c231f82
yuchen <116358@qq>
2023-10-21 11:23:18 +0800

    Add another VMC calculate function.

---
+++ rm_legged_balance_control_ros/include/rm_legged_balance_control_ros/vmc/leg_pos.h
@@ -0,0 +1,31 @@
+/*
+ * File: leg_pos.h
+ *
+ * MATLAB Coder version            : 5.2
+ * C/C++ source code generated on  : 20-Oct-2023 20:45:56
+ */
+
+#ifndef LEG_POS_H
+#define LEG_POS_H
+
+/* Include Files */
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void leg_pos(double phi1, double phi4, double pos[2]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/*
+ * File trailer for leg_pos.h
+ *
+ * [EOF]
+ */
---
+++ rm_legged_balance_control_ros/include/rm_legged_balance_control_ros/vmc/leg_spd.h
@@ -0,0 +1,31 @@
+/*
+ * File: leg_spd.h
+ *
+ * MATLAB Coder version            : 5.2
+ * C/C++ source code generated on  : 20-Oct-2023 20:45:56
+ */
+
+#ifndef LEG_SPD_H
+#define LEG_SPD_H
+
+/* Include Files */
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void leg_spd(double dphi1, double dphi4, double phi1, double phi4, double spd[2]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/*
+ * File trailer for leg_spd.h
+ *
+ * [EOF]
+ */
