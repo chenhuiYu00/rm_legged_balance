fd5f70c7924c81b72f4fbfa2f41925278893f45e
yuchen <116358@qq>
2023-10-30 21:25:10 +0800

    Use chassis cmd control.

--- rm_legged_balance_control_ros/src/synchronized_module/RosReferenceManager.cpp
+++ rm_legged_balance_control_ros/src/synchronized_module/RosReferenceManager.cpp
@@ -33,84 +33,67 @@ void RosReferenceManager::preSolverRun(ocs2::scalar_t initTime, ocs2::scalar_t f
     ocs2::vector_array_t inputTrajectory;
     scalar_t horizon = finalTime - initTime;
 
-    //    if (chassisCmd_.mode == rm_msgs::ChassisCmd::RAW) {
-    //      const size_t sample = 20;
-    //      const scalar_t dt = horizon / sample;
-    //      if (tfVel(chassisCmd_.command_source_frame, "odom", vel)) {
-    //        for (size_t i = 0; i < sample + 1; ++i) {
-    //          scalar_t time = dt * static_cast<scalar_t>(i);
-    //          const vector_t targetState = [&]() {
-    //            vector_t targetState = vector_t::Zero(rm::STATE_DIM);
-    //            // TODO: targetState(0)?
-    //            targetState(4) = initState(4) + cmdVel_.angular.z * time;
-    //            targetState(5) = cos(targetState(4)) * vel.x + sin(targetState(4)) * vel.y;
-    //            targetState(9) = cmdVel_.angular.z;
-    //            return targetState;
-    //          }();
-    //          timeTrajectory.push_back(initTime + time);
-    //          stateTrajectory.push_back(targetState);
-    //          inputTrajectory.emplace_back(vector_t::Zero(INPUT_DIM));
-    //        }
-    //        referenceManagerPtr_->setTargetTrajectories({timeTrajectory, stateTrajectory, inputTrajectory});
-    //      }
-    //      balanceControlCmdPtr_->setSitDown(false);
-    //    } else if (chassisCmd_.mode == rm_msgs::ChassisCmd::FOLLOW || chassisCmd_.mode == 3 || chassisCmd_.mode == 4) {
-    //      vector_t targetState = vector_t::Zero(rm::STATE_DIM);
-    //      if (chassisCmd_.follow_source_frame.empty()) {
-    //        chassisCmd_.follow_source_frame = "yaw";
-    //      }
-    //      try {
-    //        double roll{}, pitch{}, yaw{};
-    //        quatToRPY(buffer_.lookupTransform("odom", chassisCmd_.follow_source_frame, ros::Time(0)).transform.rotation, roll, pitch,
-    //        yaw); double yawError = angles::shortest_angular_distance(initState(4), yaw); targetState(4) = initState(4) + yawError;
-    //        balanceControlCmdPtr_->setYawError(yawError);
-    //      } catch (tf2::TransformException& ex) {
-    //        targetState(4) = initState(4);
-    //        ROS_WARN("%s", ex.what());
-    //      }
-    //      if (tfVel(chassisCmd_.command_source_frame, "base_link", vel)) {
-    //        targetState(0) = initState(0) + vel.x * horizon;
-    //        targetState(5) = vel.x;
-    //        timeTrajectory = {initTime, finalTime};
-    //        stateTrajectory.assign(2, targetState);
-    //        inputTrajectory.assign(2, vector_t::Zero(INPUT_DIM));
-    //        referenceManagerPtr_->setTargetTrajectories({timeTrajectory, stateTrajectory, inputTrajectory});
-    //        balanceControlCmdPtr_->setForwardVel(vel.x);
-    //      }
-    //      if (chassisCmd_.mode == rm_msgs::ChassisCmd::FOLLOW) {
-    //        balanceControlCmdPtr_->setMaxPitch(0.50, initTime);
-    //        balanceControlCmdPtr_->setSitDown(false);
-    //      } else if (chassisCmd_.mode == 3) {
-    //        balanceControlCmdPtr_->setMaxPitch(0.36, initTime);
-    //        balanceControlCmdPtr_->setSitDown(false);
-    //      } else if (chassisCmd_.mode == 4) {
-    //        balanceControlCmdPtr_->setSitDown(true);
-    //      }
-    //    }
-    vector_t targetState = vector_t::Zero(rm::STATE_DIM);
-    targetState(0) = initState(0) + vel.x * horizon;
-    targetState(4) = initState(4) + cmdVel_.angular.z * horizon;
-    targetState(5) = vel.x;
-    targetState(9) = vel.z;
-    balanceControlCmdPtr_->setYawError(angles::shortest_angular_distance(initState(4), initState(4) + cmdVel_.angular.z * horizon));
-    balanceControlCmdPtr_->setForwardVel(vel.x);
-
-    timeTrajectory = {initTime, finalTime};
-    stateTrajectory.assign(2, targetState);
-    inputTrajectory.assign(2, vector_t::Zero(INPUT_DIM));
-    referenceManagerPtr_->setTargetTrajectories({timeTrajectory, stateTrajectory, inputTrajectory});
+    if (chassisCmd_.mode == rm_msgs::ChassisCmd::RAW) {
+      const size_t sample = 20;
+      const scalar_t dt = horizon / sample;
+      if (tfVel(chassisCmd_.command_source_frame, "odom", vel)) {
+        for (size_t i = 0; i < sample + 1; ++i) {
+          scalar_t time = dt * static_cast<scalar_t>(i);
+          const vector_t targetState = [&]() {
+            vector_t targetState = vector_t::Zero(rm::STATE_DIM);
+            // TODO: targetState(0)?
+            targetState(4) = initState(4) + cmdVel_.angular.z * time;
+            targetState(5) = cos(targetState(4)) * vel.x + sin(targetState(4)) * vel.y;
+            targetState(9) = cmdVel_.angular.z;
+            return targetState;
+          }();
+          timeTrajectory.push_back(initTime + time);
+          stateTrajectory.push_back(targetState);
+          inputTrajectory.emplace_back(vector_t::Zero(INPUT_DIM));
+        }
+        referenceManagerPtr_->setTargetTrajectories({timeTrajectory, stateTrajectory, inputTrajectory});
+      }
+      balanceControlCmdPtr_->setSitDown(false);
+    } else if (chassisCmd_.mode == rm_msgs::ChassisCmd::FOLLOW || chassisCmd_.mode == 3 || chassisCmd_.mode == 4) {
+      vector_t targetState = vector_t::Zero(rm::STATE_DIM);
+      if (chassisCmd_.follow_source_frame.empty()) {
+        chassisCmd_.follow_source_frame = "yaw";
+      }
+      try {
+        double roll{}, pitch{}, yaw{};
+        quatToRPY(buffer_.lookupTransform("odom", chassisCmd_.follow_source_frame, ros::Time(0)).transform.rotation, roll, pitch, yaw);
+        double yawError = angles::shortest_angular_distance(initState(4), yaw);
+        targetState(4) = initState(4) + yawError;
+        balanceControlCmdPtr_->setYawError(yawError);
+      } catch (tf2::TransformException& ex) {
+        targetState(4) = initState(4);
+        ROS_WARN("%s", ex.what());
+      }
+      if (tfVel(chassisCmd_.command_source_frame, "base_link", vel)) {
+        targetState(0) = initState(0) + vel.x * horizon;
+        targetState(5) = vel.x;
+        timeTrajectory = {initTime, finalTime};
+        stateTrajectory.assign(2, targetState);
+        inputTrajectory.assign(2, vector_t::Zero(INPUT_DIM));
+        referenceManagerPtr_->setTargetTrajectories({timeTrajectory, stateTrajectory, inputTrajectory});
+        balanceControlCmdPtr_->setForwardVel(vel.x);
+      }
+      if (chassisCmd_.mode == rm_msgs::ChassisCmd::FOLLOW) {
+        balanceControlCmdPtr_->setMaxPitch(0.50, initTime);
+        balanceControlCmdPtr_->setSitDown(false);
+      } else if (chassisCmd_.mode == 3) {
+        balanceControlCmdPtr_->setMaxPitch(0.36, initTime);
+        balanceControlCmdPtr_->setSitDown(false);
+      } else if (chassisCmd_.mode == 4) {
+        balanceControlCmdPtr_->setSitDown(true);
+      }
+    }
   }
 
   if (chassisCmdUpdated_) {
     std::lock_guard<std::mutex> lock(chassisCmdMutex_);
     chassisCmdUpdated_ = false;
     balanceControlCmdPtr_->setPowerLimit(chassisCmd_.power_limit);
-
-    if (chassisCmd_.mode == 4) {
-      balanceControlCmdPtr_->setSitDown(true);
-    } else {
-      balanceControlCmdPtr_->setSitDown(false);
-    }
   }
 
   if (legCmdUpdated_) {
